2f6bedddee5c42da4dfc37343228d50631550af0
Krushil Naik <krushilnaik96@gmail.com>
2021-11-18 20:08:05 -0500

    Reorganize file structure

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
+require('dotenv');
+
 // import sequelize connection
 
 const app = express();
@@ -8,9 +10,11 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+console.log(process.env.MYSQL_DATABASE);
+
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+	console.log(`App listening on port ${PORT}!`);
 });
