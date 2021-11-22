09bdf6efe5d8aa59dd9f8dff96fdbc28dc3a7828
Krushil Naik <krushilnaik96@gmail.com>
2021-11-22 09:11:10 -0500

    Hook ORM to Express server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const routes = require('./routes');
 require('dotenv');
 
 // import sequelize connection
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -15,6 +16,8 @@ console.log(process.env.MYSQL_DATABASE);
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-	console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+	app.listen(PORT, () => {
+		console.log(`App listening on port ${PORT}!`);
+	});
 });
